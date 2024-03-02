cb4660aeb9cddb3dd563e720f8b3462d54fad233
lassmanjm <lassmanjm@gmail.com>
2024-03-02 01:51:36 +0000

    Add option to fake chat GPT call for debugging

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request, jsonify
 from PIL import Image
 from extract_recipe_text import extract_recipe
-# from fake_extract_recipe_text import extract_recipe
+from fake_extract_recipe_text import extract_recipe as fake_extract_recipe
 from flask_cors import CORS
 import re
 import json
@@ -10,18 +10,7 @@ import json
 app = Flask(__name__)
 CORS(app, resources={r"/python_server/*": {"origins": "http://localhost:5000"}})
 
-
-@app.route('/extract_recipe')
-def index():
-    return render_template('index.html')
-# def extract_json_from_response(respone
-
-@app.route('/extract_recipe/<string:recipe_json>')
-def recipe(recipe_json):
-    return render_template("recipe_template.html", recipe=recipe_json)
-
-@app.route('/extract_recipe/process', methods=['POST'])
-def process():
+def process(extract_recipe):
     if 'image' not in request.files:
         return jsonify({'message': 'No image provided'}), 400
 
@@ -41,6 +30,25 @@ def process():
         
     except Exception as e:
         return jsonify({'message': 'Error processing image: %s'%(str(e))}, 500)
+
+@app.route('/extract_recipe')
+def index():
+    return render_template('index.html')
+# def extract_json_from_response(respone
+
+@app.route('/extract_recipe/<string:recipe_json>')
+def recipe(recipe_json):
+    return render_template("recipe_template.html", recipe=recipe_json)
+
+@app.route('/extract_recipe/process_image', methods=['POST'])
+def process_image():
+    return process(extract_recipe)
+
+@app.route('/extract_recipe/fake_process_image', methods=['POST'])
+def fake_process_image():
+    return process(fake_extract_recipe)
+
+
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
