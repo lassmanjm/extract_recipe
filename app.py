5ba11d37abc11e0b6858b0e027aae72c0f852451
lassmanjm <lassmanjm@gmail.com>
2024-03-04 22:10:55 +0000

    update how fake_call is propagated

--- app.py
+++ app.py
@@ -10,17 +10,13 @@ import json
 app = Flask(__name__)
 CORS(app, resources={r"/python_server/*": {"origins": "http://localhost:5000"}})
 
-def process(extract_recipe):
-    if 'image' not in request.files:
-        return jsonify({'message': 'No image provided'}), 400
-
-    image_file = request.files['image']
+def process_image_internal(recipe_extraction_method, image_file):
     if image_file.filename == '':
         return jsonify({'message': 'No image selected'}), 400
 
     try:
         # Process the image using your Python script
-        chat_response = extract_recipe(image_file)
+        chat_response = recipe_extraction_method(image_file)
         print(chat_response)
         recipe= json.loads(chat_response)["choices"][0]["message"]["content"]
         recipe = recipe[recipe.find("{"):recipe.rfind("}")+1]
@@ -38,16 +34,27 @@ def index():
 
 @app.route('/extract_recipe/process_image', methods=['POST'])
 def process_image():
-    return process(extract_recipe)
+    print(request.files)
+    print(request.form)
+    image_file = request.files.get('image')
+    if not image_file:
+        return jsonify({'error_message': 'Error in request: No image provided.'}), 400
 
-@app.route('/extract_recipe/fake_process_image', methods=['POST'])
-def fake_process_image():
-    if 'image' not in request.files:
-        return jsonify({'error_message': 'No image provided in the request'})
-    return process(fake_extract_recipe)
-
-
-
+    advanced_options_string = request.form.get('advanced_options')
+    if not advanced_options_string:
+        return jsonify({'error_message': 'Error in request: advanced_options field not found.'})
+    try:
+        advanced_options =json.loads(advanced_options_string)
+        fake_call = advanced_options["fake_call"]
+    except Exception as e:
+        return jsonify({'error_message': 'Error in request advanced_options field: %s'%e})
+    if fake_call == 'true':
+        print(1)
+        recipe_extraction_method = fake_extract_recipe
+    else:
+        print(2)
+        recipe_extraction_method = extract_recipe
+    return process_image_internal(recipe_extraction_method, image_file)
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
